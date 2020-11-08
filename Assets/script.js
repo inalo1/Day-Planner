66270ad8a9ffe74e2f44f54342435e7808d8bd41
Ivy Gabrielle Nalo <nalo.ivielle13@gmail.com>
2020-11-07 18:46:00 -0600

    added a break on each time row

--- Assets/script.js
+++ Assets/script.js
@@ -1,2 +1,22 @@
-var nineAMInput = document.querySelector("#9am");
-console.log(nineAMInput)
+// On clicking the save button, the block converts back
+$(".saveBtn").on("click", function() {
+    // get the text from the text area
+    var text = $(this)
+        .siblings(".description")
+        .val()
+        .trim();
+    // get the selected event time
+    var selectedHourStr = $(this).siblings(".hour").text();
+    // create an object with the event to save
+    eventObj = {
+        time: selectedHourStr,
+        event: text
+    };
+
+var saveEvents = function() {
+    localStorage.setItem("events", JSON.stringify(savedEvents));
+  };
+// Display the current date
+displayDay();
+// Load saved events
+loadSavedEvents(); 
